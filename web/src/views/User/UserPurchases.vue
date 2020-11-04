bfd3a451c51451c084d752a688328b7e25a93654
Gustavo Schneider <gustavo.schneider007@gmail.com>
2020-11-04 20:31:56 -0300

    user sales and user purchases pages added

--- web/src/views/User/UserPurchases.vue
+++ web/src/views/User/UserPurchases.vue
@@ -1,15 +1,72 @@
 <template>
 	<section>
-		<h1>Minhas Compras</h1>
+		<div v-if="purchases">
+			<h2>Compras</h2>
+			<div
+				class="products-wrapper"
+				v-for="(purchase, index) in purchases"
+				:key="index"
+			>
+				<ProductItem v-if="purchase.product" :product="purchase.product">
+					<p class="seller">
+						<span>Vendedor: </span>
+						{{purchase.seller_id}}
+					</p>
+				</ProductItem>
+			</div>
+		</div>
 	</section>
 </template>
 
 <script>
+import ProductItem from '@/components/Products/ProductItem.vue';
+import api from '@/services/api';
+import { mapState } from 'vuex';
+
 export default {
 	name: 'UserPurchases',
+	components: {
+		ProductItem,
+	},
+	data() {
+		return {
+			purchases: null,
+		};
+	},
+	computed: {
+		...mapState(['user', 'login']),
+	},
+	methods: {
+		getPurchases() {
+			api.get(`/transaction?buyer_id=${this.user.id}`)
+				.then((response) => {
+					this.purchases = response.data;
+				});
+		},
+	},
+	watch: {
+		login() {
+			this.getPurchases();
+		},
+	},
+	created() {
+		if (this.login) {
+			this.getPurchases();
+		}
+	},
 };
 </script>
 
-<style>
+<style scoped>
+.product-wrapper {
+	margin-bottom: 40px;
+}
 
+.seller span {
+	color: #E80;
+}
+
+h2 {
+	margin-bottom: 20px;
+}
 </style>
--- web/src/views/User/UserSales.vue
+++ web/src/views/User/UserSales.vue
@@ -1,15 +1,91 @@
 <template>
 	<section>
-		<h1>Minhas Vendas</h1>
+		<div v-if="sales">
+			<h2>Vendas</h2>
+			<div
+				class="products-wrapper"
+				v-for="(sale, index) in sales"
+				:key="index"
+			>
+				<ProductItem v-if="sale.product" :product="sale.product">
+					<p class="buyer">
+						<span>Comprador: </span>
+						{{sale.buyer_id}}
+					</p>
+				</ProductItem>
+				<div class="delivery">
+					<h3>Entrega:</h3>
+					<ul v-if="sale.address">
+						<li v-for="(value, key) in sale.address" :key="key">
+							{{key}}: {{value}}
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
 
 <script>
+import ProductItem from '@/components/Products/ProductItem.vue';
+import api from '@/services/api';
+import { mapState } from 'vuex';
+
 export default {
 	name: 'UserSales',
+	components: {
+		ProductItem,
+	},
+	data() {
+		return {
+			sales: null,
+		};
+	},
+	computed: {
+		...mapState(['user', 'login']),
+	},
+	methods: {
+		getSales() {
+			api.get(`/transaction?seller_id=${this.user.id}`)
+				.then((response) => {
+					this.sales = response.data;
+				});
+		},
+	},
+	watch: {
+		login() {
+			this.getSales();
+		},
+	},
+	created() {
+		if (this.login) {
+			this.getSales();
+		}
+	},
 };
 </script>
 
 <style>
+.product-wrapper {
+	margin-bottom: 40px;
+}
 
+.buyer span {
+	color: #E80;
+}
+
+h2 {
+	margin-bottom: 20px;
+}
+
+h3 {
+	margin: 0;
+	justify-self: end;
+}
+
+.delivery {
+	display: grid;
+	grid-template-columns: minmax(100px, 200px) 1fr;
+	grid-gap: 20px;
+}
 </style>
