2c4346dabd2cb5a0902d1f413fdb2fc126f6d764
SttavoS <gustavo@gustavoschneider.com>
2020-12-20 22:34:00 -0300

    add PHP Insights

--- app/Http/Middleware/apiProtectedRoute.php
+++ app/Http/Middleware/apiProtectedRoute.php
@@ -20,12 +20,12 @@ class apiProtectedRoute extends BaseMiddleware
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
-        } catch (\Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+        } catch (\Exception $error) {
+            if ($error instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+            } else if ($error instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json(['status' => 'Token is Expired']);
-            }else{
+            } else {
                 return response()->json(['status' => 'Authorization Token not found']);
             }
         }
